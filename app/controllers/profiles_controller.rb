49424b3e10c2b9664ffead6b89844401f3af73bd
Quitterie Cadiou <quitterie.cadiou@gmail.com>
2018-10-29 14:45:16 +0100

    created links between dashboard and recipe show

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,16 +1,23 @@
 class ProfilesController < ApplicationController
   def new
+    @profile = Profile.new
   end
 
   def create
+
+    redirect_to profile_dashboard_path(current_user)
   end
 
   def show
-  end
 
-  def update
   end
 
   def edit
+    @profile = Profile.find(current_user.id)
+  end
+
+  def update
+
+    redirect_to profile_dashboard_path(current_user)
   end
 end
