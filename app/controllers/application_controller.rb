5f4be0f1bde57c21693327f8c19edfc3b04eee55
Quitterie Cadiou <quitterie.cadiou@gmail.com>
2018-10-29 18:09:11 +0100

    modified navbar, created routes to personal details

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource)
+    profile_dashboard_path(Profile.where(user: current_user).first)
+  end
 end
