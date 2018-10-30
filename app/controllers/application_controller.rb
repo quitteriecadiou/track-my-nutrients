c319440e654df4351cd1479b2ba1d806fa3b37f6
Quitterie Cadiou <quitterie.cadiou@gmail.com>
2018-10-30 15:46:27 +0100

    created seeds for food items, categories and diets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
+  NUTRIENTS = ["protein", "carbohydrate", "fat", "sugars", "fibres", "fa_saturated", "fa_mono", "fa_poly", "cholesterol", "salt", "calcium", "copper", "iron", "magnesium", "manganese", "phosphorus", "potassium", "sodium", "zinc", "retinol", "beta_carotene", "vitamin_d", "vitamin_e", "vitamin_c", "vitamin_b1", "vitamin_b2", "vitamin_b3", "vitamin_b5", "vitamin_b6", "vitamin_b9", "vitamin_b12"]
+
   def after_sign_in_path_for(resource)
     profile_dashboard_path(Profile.where(user: current_user).first)
   end
+
 end
