f8a71a49b1b62941c4b1aee7af6282884809971e
laury bretteville <laury.bretteville@edhec.com>
2018-10-30 18:41:27 +0100

    routes modified

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  include Pundit
 
   def after_sign_in_path_for(resource)
     profile_dashboard_path(Profile.where(user: current_user).first)
