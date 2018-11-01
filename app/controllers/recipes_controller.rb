b9df50ca775a752345e4011ac54d43bdaf2523d0
laury bretteville <laury.bretteville@edhec.com>
2018-11-01 16:42:54 +0100

    updated recipes index + filter bar

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,12 @@
 class RecipesController < ApplicationController
   def index
+    if params[:query].present?
+      @recipes = Recipe.where("name ILIKE ?", "%#{params[:query]}%")
+    elsif params[:category].present?
+      @recipes = Recipe.where(name: "#{params[:category]}")
+    else
     @recipes = Recipe.all
+    end
   end
 
   def show
