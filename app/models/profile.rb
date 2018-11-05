e4f389a84a990e1abff572e8af260c8f70eebd3d
Quitterie Philipon <quitterie.philipon@edhec.com>
2018-11-05 10:24:29 +0100

    updated the suggested recipes method

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -12,11 +12,11 @@ class Profile < ApplicationRecord
   validates :weight, presence: true
   validates :gender, inclusion: { in: ['Female', 'Male'] }
 
-  def suggested_recipes(added_recipes)
+  def suggested_recipes(added_recipes, recipes)
     tracker = AddedRecipe.tracker(added_recipes)
 
 
-    Recipe.where("protein_per_portion <= ? and carbohydrate_per_portion <= ? and calcium_per_portion <= ? and sodium_per_portion <= ?",
+    recipes.where("protein_per_portion <= ? and carbohydrate_per_portion <= ? and calcium_per_portion <= ? and sodium_per_portion <= ?",
                   personal_diet[:protein_obj_personal] - tracker[:protein] < 0 ? 0 : personal_diet[:protein_obj_personal] - tracker[:protein],
                   personal_diet[:carbohydrate_obj_personal] - tracker[:carbohydrate] < 0 ? 0 : personal_diet[:carbohydrate_obj_personal] - tracker[:carbohydrate],
                   personal_diet[:calcium_obj_personal] - tracker[:calcium] < 0 ? 0 : personal_diet[:calcium_obj_personal] - tracker[:calcium],
@@ -24,4 +24,3 @@ class Profile < ApplicationRecord
   end
 
 end
-
