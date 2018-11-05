1dadba775883d463f00beee7aa8a47c7543327a0
Quitterie Philipon <quitterie.philipon@edhec.com>
2018-11-05 09:47:16 +0100

    suggested-recipes card with js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,10 +2,9 @@ import "bootstrap";
 import Chart from 'chart.js';
 
 
-import { showsDietCard } from '../components/personal_info_form';
 import { circleTracker } from '../components/circle_tracker';
 import { suggestedRecipesShowNutrients } from '../components/suggested_recipes';
 
-//showsDietCard();
+
 circleTracker();
 suggestedRecipesShowNutrients();
