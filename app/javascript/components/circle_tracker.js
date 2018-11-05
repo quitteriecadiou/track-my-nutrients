2060ed9af766585211e65e525ce2395ff49d87fd
Quitterie Philipon <quitterie.philipon@edhec.com>
2018-11-05 18:42:50 +0100

    circle tracker working + suggested recipe go back button wip

--- app/javascript/components/circle_tracker.js
+++ app/javascript/components/circle_tracker.js
@@ -1,14 +1,75 @@
+
+
 function circleTracker() {
-  var ctx = document.getElementById("circle-tracker-animation");
-  var circleTrackerTry = new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-      datasets: [{
-        backgroundColor: ['#27AE60', '#ffffff'],
-        data: [50, 50]
-      }],
-    },
-  });
+  const nutrients = document.querySelectorAll(".single-tracker")
+
+    const nutrientTitle = document.querySelector(".tracker-nutrient-name").innerHTML
+    nutrients[0].querySelector(".tracker-nutrient-name").classList.add("selected");
+
+    const circleTrackerName = document.querySelector(".circle-tracker-title-nutrient")
+    circleTrackerName.innerHTML = nutrientTitle
+
+    const nutrientPercent = nutrients[0].querySelector(".progress-bar-label").innerHTML
+    const nutrientPercentWithoutSign = nutrientPercent.substring(0,nutrientPercent.length-1)
+
+    const trackerDiv = document.querySelector('.circle-tracker-progress')
+    trackerDiv.innerHTML = '<canvas id="circle-tracker-animation"></canvas>'
+
+    var ctx = document.getElementById("circle-tracker-animation");
+    var circleTrackerTry = new Chart(ctx, {
+      type: 'doughnut',
+      data: {
+        datasets: [{
+          backgroundColor: ['#27AE60', '#F5F5F5'],
+          data: [nutrientPercentWithoutSign, 100 - nutrientPercentWithoutSign]
+        }],
+      },
+      options: {
+        animation: {
+          animateRotate: true
+        }
+      },
+    });
+
+  nutrients.forEach((nutrient) => nutrient.addEventListener("click", event => {
+
+    event.preventDefault();
+
+    const nutrientsTitle = document.querySelectorAll(".tracker-nutrient-name")
+    nutrientsTitle.forEach((nutrientTitle) => {
+      nutrientTitle.classList.remove("selected");
+    });
+
+    const nutrientTitle = nutrient.querySelector(".tracker-nutrient-name").innerHTML
+
+    nutrient.querySelector(".tracker-nutrient-name").classList.add("selected");
+
+    const circleTrackerName = document.querySelector(".circle-tracker-title-nutrient")
+    circleTrackerName.innerHTML = nutrientTitle
+
+    const nutrientPercent = nutrient.querySelector(".progress-bar-label").innerHTML
+    const nutrientPercentWithoutSign = nutrientPercent.substring(0,nutrientPercent.length-1)
+
+    const trackerDiv = document.querySelector('.circle-tracker-progress')
+    trackerDiv.innerHTML = '<canvas id="circle-tracker-animation"></canvas>'
+
+    var ctx = document.getElementById("circle-tracker-animation");
+    var circleTrackerTry = new Chart(ctx, {
+      type: 'doughnut',
+      data: {
+        datasets: [{
+          backgroundColor: ['#27AE60', '#F5F5F5'],
+          data: [nutrientPercentWithoutSign, 100 - nutrientPercentWithoutSign]
+        }],
+      },
+      options: {
+        animation: {
+          animateRotate: true
+        }
+      },
+    });
+  }))
+
 };
 
 export { circleTracker };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,9 @@ import Chart from 'chart.js';
 
 import { circleTracker } from '../components/circle_tracker';
 import { suggestedRecipesShowNutrients } from '../components/suggested_recipes';
+import { backToSuggestedRecipeList } from '../components/suggested_recipes';
 
 
 circleTracker();
 suggestedRecipesShowNutrients();
+backToSuggestedRecipeList();
