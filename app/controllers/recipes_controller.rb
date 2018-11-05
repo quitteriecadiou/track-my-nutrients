d5821ce37e529af5044a059f4071a699bd92b380
Quitterie Cadiou <quitterie.cadiou@gmail.com>
2018-11-05 18:35:55 +0100

    finished forms, problems with ing unit view, pb with photo

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -52,7 +52,26 @@ class RecipesController < ApplicationController
 
   def edit
     @recipe = Recipe.find(params[:id])
+    @recipe.prep_time = nil if @recipe.prep_time == "Unknown"
+    @recipe.difficulty = nil if @recipe.difficulty == "Unknown"
+    @recipe.description = nil if @recipe.description == "No instructions available"
     @ingredient = Ingredient.new
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+
+    if @recipe.update(recipe_params)
+
+      @recipe.prep_time = "Unknown" if @recipe.prep_time == ""
+      @recipe.difficulty = "Unknown" if @recipe.difficulty == ""
+      @recipe.description = "No instructions available" if @recipe.description == ""
+      @recipe.save
+
+      redirect_to recipe_path(@recipe)
+    else
+      render :edit
+    end
   end
 
   private
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,9 +2,12 @@ import "bootstrap";
 import Chart from 'chart.js';
 
 
+import { selectedIng } from '../recipes/edit';
 import { showsDietCard } from '../components/personal_info_form';
 import { circleTracker } from '../components/circle_tracker';
 
 
+selectedIng();
 showsDietCard();
 circleTracker();
+
