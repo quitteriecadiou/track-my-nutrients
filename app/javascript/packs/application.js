3c0cfd25c0506aeb93804b279cb21e7c3ee644b2
Quitterie Cadiou <quitterie.cadiou@gmail.com>
2018-11-06 13:29:11 +0100

    select2 works and new photo recipe placeholder

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,16 +4,22 @@ import Chart from 'chart.js';
 
 
 import { selectedIng } from '../recipes/edit';
-import { circleTracker } from '../components/circle_tracker';
-import { suggestedRecipesShowNutrients } from '../components/suggested_recipes';
-import { backToSuggestedRecipeList } from '../components/suggested_recipes';
-import { selectedDiet } from '../components/personal_info_form';
+// import { circleTracker } from '../components/circle_tracker';
+// import { suggestedRecipesShowNutrients } from '../components/suggested_recipes';
+// import { backToSuggestedRecipeList } from '../components/suggested_recipes';
+// import { selectedDiet } from '../components/personal_info_form';
+import '../components/select2';
+import { initForm } from '../components/select2';
 
 
 
 
 selectedIng();
-circleTracker();
-suggestedRecipesShowNutrients();
-backToSuggestedRecipeList();
-selectedDiet();
+initForm();
+// circleTracker();
+// suggestedRecipesShowNutrients();
+// backToSuggestedRecipeList();
+// selectedDiet();
+
+
+window.initForm = initForm;
